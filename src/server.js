a016b7d2c918a387aa8c0868084f77cdc3eb7f26
JuanchoD17 <juandiegoarevalo2@gmail.com>
2021-12-01 12:09:39 -0500

    Se realizo el archivo de utils de verificacion del token

---
+++ src/server.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    auth_api_url: 'https://innovatech-mintic.herokuapp.com/',
+    producto_api_url: 'https://innovatech-mintic-productos.herokuapp.com/',
+    shoppincart_api_url: ''
+
+
+};
---
+++ src/utils/Authentication.js
@@ -0,0 +1,42 @@
+const {ApolloError} = require ('apollo-server')
+const serverconfig  = require ('../server')
+const fetch         = require ('node-fetch');
+const { Body } = require('apollo-server-env');
+
+
+const authentication = async ( {req} ) =>{
+
+    const token = req.headers.authorization  || '';
+    
+    if(token == '')
+        return{userIdToken: null}
+
+
+    else{
+        try{
+            let requestOptions = {
+
+                methond : 'POST', headers:{"content-type":"application/json"},
+                body: JSON.stringify( {token} ), redirect: 'follow'
+            };
+
+            let response = await fetch(`${serverconfig.auth_api_url}/verifyToken/`,
+                requestOptions)
+
+        if (response.status != 200){
+
+            console.log(response)
+
+            throw new ApolloError(`SECION INACTIVA ${401}`+ response.status, 401)
+        }
+        return{userIdToken: (await response.JSON()).UserId};
+        }
+        catch (console){
+
+            throw new ApolloError(`TOKEN ERROR ${500}: ${error}`, 500)
+
+        }
+        
+    }
+    
+}
